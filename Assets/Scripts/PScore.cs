129353e3e01614489b235448edef6a9d18665d43
Sang Hyeon <rlatkdgus500@gmail.com>
2017-07-05 13:16:38 +0900

    Add Score and gyeol button

---
+++ Assets/Scripts/PScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class PScore : MonoBehaviour {
+
+    private MGameBoard model;
+
+    [SerializeField]
+    private Text scoreText;
+
+    [SerializeField]
+    private Text comboText;
+
+    private void Awake() {
+    }
+
+    private void Start() {
+        model = MGameBoard.getInstance();
+
+        model.score.AsObservable()
+            .Subscribe(Score => {
+                scoreText.text = Score.ToString();
+            });
+
+        model.combo.AsObservable()
+            .Subscribe(Combo => {
+                comboText.text = Combo.ToString();
+            });
+
+    }
+}
