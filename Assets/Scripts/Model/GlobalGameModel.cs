20a7041b7b75d84aec720a019f2d46d13ecd1a52
Sang Hyeon <rlatkdgus500@gmail.com>
2019-05-04 21:41:55 +0900

    #Common #Add some new scripts

---
+++ Assets/Scripts/Model/GlobalGameModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New GlobalGameModel", menuName = "ScriptableObject/GlobalGameModel")]
+public class GlobalGameModel : SingletonScriptableObject<GlobalGameModel> {
+
+
+}
---
+++ Assets/Scripts/View/ScoreBoardView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class ScoreBoardView : MonoBehaviour {
+
+    public Text scoreText;
+    public Text comboText;
+
+    public void SetupScore(int _score) {
+
+        scoreText.text = $"{_score} Point";
+    }
+
+    public void SetupCombo(int _combo) {
+
+        comboText.text = $"{_combo} Combo";
+    }
+
+    public void UpdateView(int _score, int _combo) {
+
+        SetupScore(_score);
+        SetupCombo(_combo);
+    }
+}
