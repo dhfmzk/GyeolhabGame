49fe623ab008ad77967e4bb3e96a5185b1974d1f
Sang Hyeon <rlatkdgus500@gmail.com>
2019-05-04 21:54:21 +0900

    #Common #Add ranking data in global asset (for saving data)

---
+++ Assets/Scripts/Common/CustomTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Score Data
+[Serializable]
+public class ScoreData {
+
+    public int score;
+    public int maxCombo;
+    public int playtime;
+}
--- Assets/Scripts/Model/GlobalGameModel.cs
+++ Assets/Scripts/Model/GlobalGameModel.cs
@@ -6,5 +6,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New GlobalGameModel", menuName = "ScriptableObject/GlobalGameModel")]
 public class GlobalGameModel : SingletonScriptableObject<GlobalGameModel> {
 
-
+    public int maxListSize = 10;
+    public List<ScoreData> rankingList;
 }
