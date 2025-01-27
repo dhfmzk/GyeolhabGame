18704b766f2d06b331bd55eb4c61298c9d8e5f2f
Orca <rlatkdgus500@gmail.com>
2025-01-28 07:24:05 +0900

    💄 Wiring Main menu

---
+++ Assets/Scripts/Component/MainMenuComponent.cs
@@ -0,0 +1,35 @@
+﻿using GameSystem;
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Component
+{
+    public class MainMenuComponent : MonoBehaviour
+    {
+        private static readonly int IsActive = Animator.StringToHash("IsActive");
+
+        [Header("View")]
+        public Button startButton;
+        public Animator animator;
+        
+        private void Start()
+        {
+            this.startButton.OnClickAsObservable()
+                .Subscribe(e =>
+                {
+                    GameLoop.I.GameModel.StartGame();
+                })
+                .AddTo(this);
+        }
+
+        private void Update()
+        {
+            var isActive = this.animator.GetBool(IsActive);
+            if (!GameLoop.I.GameModel.IsGamePlaying != this.animator.GetBool(IsActive))
+            {
+                this.animator.SetBool(IsActive, !GameLoop.I.GameModel.IsGamePlaying);
+            }
+        }
+    }
+}
